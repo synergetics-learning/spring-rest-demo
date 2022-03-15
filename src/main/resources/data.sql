b45f6271e18b30e4aba8657d93533a95a152159d
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2022-03-15 15:14:50 +0530

    Spring REST Demo

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT into BOOKS ( book_author, book_status, title )
+VALUES ('Yashwant Kanetkar', 'A', 'Let Us C'), 
+VALUES ('Yashwant Kanetkar', 'A', 'Let Us C++');
+
+INSERT into MEMBERS (first_name, last_name, member_status)
+VALUES ('Mahendra','Shinde','R'),
+VALUES ('Sonali','Shinde','R');
+
+INSERT into book_issues (book_id, member_id, issue_date, return_date_exp,  issue_status)
+VALUES (1, 2, '2022-03-04','2022-03-12','P' );
+
